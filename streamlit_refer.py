9b29d5bbeb54e1b279bf143da8114a6377c78e61
git-psj <20211485@daejin.ac.kr>
2024-07-26 07:20:52 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -131,7 +131,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore, google_api_key):
     try:
-        llm = ChatGoogleGenerativeAI(google_api_key=google_api_key, model='gemini-1.5-flash')
+        llm = ChatGoogleGenerativeAI(google_api_key=google_api_key, model='gemini-1.5-pro')
         conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
